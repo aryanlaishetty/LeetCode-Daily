7049272a22372a7cbd2f0c3bc026e787653bd6c0
Aryan Laishetty <129854148+aryanlaishetty@users.noreply.github.com>
2026-01-08 20:16:44 +0530

    Time: 11 ms (20.91%) | Memory: 26.3 MB (35.90%) - LeetSync

---
+++ LeetCode-Solvings/229-majority-element-ii/majority-element-ii.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    vector<int> majorityElement(vector<int>& nums) {
+        unordered_map<int, int> m;  //<nums[i], frequence>
+        vector<int> ans;
+
+        for(int i=0; i<nums.size(); i++) {
+            if(m.count(nums[i])) {
+                m[nums[i]]++;
+            } else {
+                m[nums[i]] = 1;
+            }
+        }
+
+        for(pair<int, int> p : m) {     //<key=nums[i], freq>
+            if(p.second > nums.size()/3) {
+                ans.push_back(p.first);
+            }
+        }
+        return ans;
+    }
+};
