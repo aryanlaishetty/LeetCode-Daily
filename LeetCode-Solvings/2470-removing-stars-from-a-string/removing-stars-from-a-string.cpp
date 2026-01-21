649c4970eedf5be8ba71e51f9bd43b2e36ba07ef
Aryan Laishetty <129854148+aryanlaishetty@users.noreply.github.com>
2026-01-21 19:04:00 +0530

    Time: 35 ms (8.35%) | Memory: 29.8 MB (33.84%) - LeetSync

---
+++ LeetCode-Solvings/2470-removing-stars-from-a-string/removing-stars-from-a-string.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    string removeStars(string s) {
+        stack<char> st;
+        string ans = "";
+
+        for(int i=0; i<s.size(); i++) {
+            if(isalpha(s[i])) {
+                st.push(s[i]);
+            } else {
+                st.pop();
+            }
+        }
+
+        while(!st.empty()) {
+            ans += st.top();
+            st.pop();
+        }
+
+        reverse(ans.begin(), ans.end());
+        
+        return ans;
+    }
+};
