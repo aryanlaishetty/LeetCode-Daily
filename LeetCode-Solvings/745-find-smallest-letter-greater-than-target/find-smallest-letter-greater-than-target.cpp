f49a31fa33b91b36c0d3a9575ba0cebc770d491e
Aryan Laishetty <129854148+aryanlaishetty@users.noreply.github.com>
2026-01-31 17:54:08 +0530

    Time: 0 ms (100.00%) | Memory: 19.8 MB (62.61%) - LeetSync

---
+++ LeetCode-Solvings/745-find-smallest-letter-greater-than-target/find-smallest-letter-greater-than-target.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    char nextGreatestLetter(vector<char>& letters, char target) {
+        for(char ch : letters) {
+            if(ch > target) {
+                return ch;
+            }
+        }
+
+        return (char)letters[0];
+    }
+};
