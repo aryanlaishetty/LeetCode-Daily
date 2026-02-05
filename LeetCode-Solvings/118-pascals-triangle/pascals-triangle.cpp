815c1e688034f1d60e1cb41fbd6a71a66f3a0442
Aryan Laishetty <129854148+aryanlaishetty@users.noreply.github.com>
2026-02-05 10:55:52 +0530

    Time: 0 ms (100.00%) | Memory: 9.6 MB (89.38%) - LeetSync

---
+++ LeetCode-Solvings/118-pascals-triangle/pascals-triangle.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    //Optimal Approach
+    //tc = O(n*n)
+    //sc = O(1)
+    vector<int> generateRow(int row) {
+        vector<int> res;
+        
+        int ans = 1;
+        res.push_back(ans);
+        
+        for(int i=1; i<=row; i++) {
+            ans *= (row-i+1);
+            ans /= (i);
+            res.push_back(ans);
+        }
+        
+        return res;
+    }
+
+    vector<vector<int>> generate(int numRows) {
+        vector<vector<int>> triangle;
+    
+        for(int i=0; i<numRows; i++) {
+            triangle.push_back(generateRow(i));
+        }
+        
+        return triangle;
+    }
+};
