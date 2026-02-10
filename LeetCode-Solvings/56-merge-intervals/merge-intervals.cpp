acb21fb5e0221e8c171933ca9ae9ecd8843d4ee8
Aryan Laishetty <129854148+aryanlaishetty@users.noreply.github.com>
2026-02-10 11:33:23 +0530

    Time: 5 ms (52.94%) | Memory: 23.9 MB (58.81%) - LeetSync

---
+++ LeetCode-Solvings/56-merge-intervals/merge-intervals.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    //Optimal Approach
+    //tc = O(nlogn + n)
+    //sc = O(n)
+    vector<vector<int>> merge(vector<vector<int>>& intervals) {
+        vector<vector<int>> ans;
+
+        sort(intervals.begin(), intervals.end());   
+
+        for(int i=0; i<intervals.size(); i++) {
+            if(ans.empty() || ans.back()[1] < intervals[i][0]) {
+                ans.push_back(intervals[i]);
+            } else {
+                ans.back()[1] = max(ans.back()[1], intervals[i][1]);
+            }
+        }
+
+        return ans;
+    }
+};
