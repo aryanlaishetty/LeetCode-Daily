300ae212c87c86aeb96c72d5f15930464cb3615f
Aryan Laishetty <129854148+aryanlaishetty@users.noreply.github.com>
2026-02-11 10:24:18 +0530

    Time: 0 ms (100.00%) | Memory: 12.3 MB (40.53%) - LeetSync

--- LeetCode-Solvings/88-merge-sorted-array/merge-sorted-array.cpp
+++ LeetCode-Solvings/88-merge-sorted-array/merge-sorted-array.cpp
@@ -1,32 +1,17 @@
 class Solution {
 public:
+    //Better Approach
+    //tc = O(m+n) + O(mlogm)
+    //sc = O(1)
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
-        int i=0, j=0, k=0;
-        vector<int> temp(m+n, 0);
+        int j = 0;
 
-        while(i!=m && j!=n) {
-            if(nums1[i] < nums2[j]) {
-                temp[k] = nums1[i];
-                i++, k++;
-            } else {
-                temp[k] = nums2[j];
-                j++, k++;
+        for(int i=0; i<nums1.size(); i++) {
+            if(nums1[i] == 0 && j < n) {
+                nums1[i] = nums2[j++];
             }
         }
 
-        while(i != m) {
-            temp[k] = nums1[i];
-            i++, k++;
-        }
-
-        while(j != n) {
-            temp[k] = nums2[j];
-            j++, k++;
-        }
-
-
-        for(int a=0; a<temp.size(); a++) {
-            nums1[a] = temp[a];
-        }
+        sort(nums1.begin(), nums1.end());
     }
 };
