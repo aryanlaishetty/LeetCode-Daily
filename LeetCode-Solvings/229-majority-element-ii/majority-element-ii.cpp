1938a7f318e5b51dcddd97e44e99cde0b03179e3
Aryan Laishetty <129854148+aryanlaishetty@users.noreply.github.com>
2026-02-15 11:29:58 +0530

    Time: 0 ms (100.00%) | Memory: 23.7 MB (63.86%) - LeetSync

--- LeetCode-Solvings/229-majority-element-ii/majority-element-ii.cpp
+++ LeetCode-Solvings/229-majority-element-ii/majority-element-ii.cpp
@@ -1,22 +1,35 @@
 class Solution {
 public:
     vector<int> majorityElement(vector<int>& nums) {
-        unordered_map<int, int> m;  //<nums[i], frequence>
-        vector<int> ans;
+        int count1 = 0, count2 = 0;
+        int el1 = INT_MIN, el2 = INT_MIN;
 
         for(int i=0; i<nums.size(); i++) {
-            if(m.count(nums[i])) {
-                m[nums[i]]++;
-            } else {
-                m[nums[i]] = 1;
+            if(count1 == 0 && nums[i] != el2) {
+                count1 = 1;
+                el1 = nums[i];
+            } else if(count2 == 0 && nums[i] != el1) {
+                count2 = 1;
+                el2 = nums[i];
+            } else if(nums[i] == el1) {
+                count1++;
+            } else if(nums[i] == el2) {
+                count2++;
+            } else {    //not equal to both el1 and el2 
+                count1--, count2--;
             }
         }
 
-        for(pair<int, int> p : m) {     //<key=nums[i], freq>
-            if(p.second > nums.size()/3) {
-                ans.push_back(p.first);
-            }
+        count1 = 0, count2 = 0;
+        for(int i=0; i<nums.size(); i++) {
+            if(nums[i] == el1) count1++;
+            else if(nums[i] == el2) count2++;
         }
+
+        vector<int> ans;
+        if(count1 > nums.size()/3) ans.push_back(el1);
+        if(count2 > nums.size()/3) ans.push_back(el2);
+
         return ans;
     }
 };
