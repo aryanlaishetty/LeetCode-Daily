4f6b3c9af0445daa6c89398bcf1baca04ecbf410
Aryan Laishetty <129854148+aryanlaishetty@users.noreply.github.com>
2026-03-10 00:10:58 +0530

    Time: 5 ms (65.36%) | Memory: 14.8 MB (99.42%) - LeetSync

--- LeetCode-Solvings/138-copy-list-with-random-pointer/copy-list-with-random-pointer.cpp
+++ LeetCode-Solvings/138-copy-list-with-random-pointer/copy-list-with-random-pointer.cpp
@@ -16,28 +16,41 @@ public:
 
 class Solution {
 public:
-    //brute force approach - using hashmaps
-    //tc = O(n + n)
-    //sc = O(n + n), one of the 'n' is not reduceable 
+    //Optimal approach - inserting nodes in between existing nodes
+    //tc = O(n + n + n)
+    //sc = O(n), 'n' is not reducible and no extra space is used
     Node* copyRandomList(Node* head) {
         Node* temp = head;
-        map<Node*, Node*> m;
-
+        
+        //insert nodes in between, here next pointer is automatically assigned
         while(temp != NULL) {
-            Node* newNode = new Node(temp->val);
-            m[temp] = newNode;
-            temp = temp->next;
+            Node* copyNode = new Node(temp->val);
+            copyNode->next = temp->next;
+            temp->next = copyNode;
+            temp = temp->next->next;
         }
 
+        //connect random pointers
         temp = head;
 
         while(temp != NULL) {
-            Node* copyNode = m[temp];
-            copyNode->next = m[temp->next];
-            copyNode->random = m[temp->random];
-            temp = temp->next;
+            Node* copyNode = temp->next;
+            if(temp->random) copyNode->random = temp->random->next;
+            temp = temp->next->next;
         }
 
-        return m[head];
+        //unlink inbetween nodes and rearrange them properly
+        temp = head;
+        Node* dummy = new Node(-1);
+        Node* res = dummy;
+
+        while(temp != NULL) {
+            res->next = temp->next;
+            temp->next = res->next->next;
+            res = res->next;
+            temp = temp->next;
+        }
+        
+        return dummy->next;
     }
 };
